54fdfda9b9024f3a91b0c83583c45592c2969a54
Chris <nttriet9a4@gmail.com>
2023-06-29 15:34:18 +0700

    feat: codec

--- src/common/codec/index.ts
+++ src/common/codec/index.ts
@@ -1,8 +1,28 @@
-import { AddressType, BinaryCodec, U32Type } from "@multiversx/sdk-core/out";
+import {
+  AddressType,
+  BigUIntType,
+  BinaryCodec,
+  U32Type,
+  U8Type,
+} from "@multiversx/sdk-core/out";
 
 export function decodeU32(str: string): number {
   return new BinaryCodec()
     .decodeTopLevel(Buffer.from(str, "base64"), new U32Type())
+    .valueOf()
+    .toFixed(0);
+}
+
+export function decodeU8(str: string): number {
+  return new BinaryCodec()
+    .decodeTopLevel(Buffer.from(str, "base64"), new U8Type())
+    .valueOf()
+    .toFixed(0);
+}
+
+export function decodeBigUint(str: string): number {
+  return new BinaryCodec()
+    .decodeTopLevel(Buffer.from(str, "base64"), new BigUIntType())
     .valueOf()
     .toFixed(0);
 }
@@ -19,6 +39,5 @@ export function encodeBytes(bytes: Buffer): string {
 }
 
 export function encodeArrayBytes(arr: Buffer[]): string[] {
-  return arr.map((v: Buffer) => v.toString('hex'));
-
+  return arr.map((v: Buffer) => v.toString("hex"));
 }
